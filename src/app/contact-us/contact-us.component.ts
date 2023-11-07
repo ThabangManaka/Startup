b1875919ad0888f39efb56eb672b8b7aafc099c7
ThabangManaka <thabangdansteyn@gmail.com>
2023-11-07 12:29:03 +0200

    Added contact form

--- src/app/contact-us/contact-us.component.ts
+++ src/app/contact-us/contact-us.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
-
+import { faPhone } from '@fortawesome/free-solid-svg-icons';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { faMobile} from '@fortawesome/free-solid-svg-icons';
+import { faMapMarkedAlt} from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-contact-us',
   templateUrl: './contact-us.component.html',
   styleUrls: ['./contact-us.component.scss']
 })
 export class ContactUsComponent {
-
+  faPhone  = faPhone
+  faEnvelope  = faEnvelope
+  faMobile =faMobile
+  faMapMarkedAlt =faMapMarkedAlt
 }
